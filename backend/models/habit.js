36ab2a22184e1e3639cff3cf002ad36c67798887
Agha Noor Ahmed Khan <150905236+AghaNoorAhmed@users.noreply.github.com>
2025-12-12 21:07:48 -0600

    Update habit.js

--- backend/models/habit.js
+++ backend/models/habit.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
 
+const habitSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  duration: { type: Number, default: 0 },
+  date: { type: Date, default: Date.now },
+  completed: { type: Boolean, default: false }
+});
+
+export default mongoose.model("Habit", habitSchema);
