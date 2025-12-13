22c4a45db1f851a3b776351be155e7b072bff626
Agha Noor Ahmed Khan <150905236+AghaNoorAhmed@users.noreply.github.com>
2025-12-12 21:17:02 -0600

    Update WeeklyChart.jsx

--- frontend/src/components/WeeklyChart.jsx
+++ frontend/src/components/WeeklyChart.jsx
@@ -1,1 +1,23 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
 
+export default function WeeklyChart() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    api.get("/habits/stats/weekly").then(res => setData(res.data));
+  }, []);
+
+  return (
+    <>
+      <h2>Weekly Progress</h2>
+      <LineChart width={400} height={250} data={data}>
+        <XAxis dataKey="day" />
+        <YAxis />
+        <Tooltip />
+        <Line dataKey="completed" stroke="#8884d8" />
+      </LineChart>
+    </>
+  );
+}
