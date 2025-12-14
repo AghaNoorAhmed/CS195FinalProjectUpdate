1cfc186c993c8713c14018e7e1b02d35b505a0e3
AghaNoorAhmed <aghanoorahmed.khan@drake.edu>
2025-12-14 03:46:41 -0600

    Add console log to debug CORS issue

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
+    console.log("Request origin:", origin);
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
